1faf66928c7dc15d720ad3c2353f228935765c4a
Shanmukh Anaparthi <110324769+Shanmukh025@users.noreply.github.com>
2023-10-16 19:35:26 +0530

    Update sample_interest.sh

--- sample_interest.sh
+++ sample_interest.sh
@@ -22,7 +22,7 @@ echo"Enter rate of interest per year:"
 read r    
 echo"Enter time period in years:"
 read t     
-
+#give simple interest formula
 s=`expr $p \* $t \* $r / 100`    
 echo"The simple interest is: "
 echo$s
